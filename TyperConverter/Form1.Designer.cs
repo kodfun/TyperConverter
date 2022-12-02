4faa4e4cb4967d968c4f690909a6c163272c5fe2
KodluYorum <info@kodluyorum.dev>
2022-12-03 02:17:25 +0300

    Add project files.

---
+++ TyperConverter/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace TyperConverter
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(12, 27);
+            this.txtInput.Multiline = true;
+            this.txtInput.Name = "txtInput";
+            this.txtInput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtInput.Size = new System.Drawing.Size(342, 411);
+            this.txtInput.TabIndex = 0;
+            this.txtInput.TextChanged += new System.EventHandler(this.txtInput_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Text";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(360, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Output";
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Location = new System.Drawing.Point(360, 27);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtOutput.Size = new System.Drawing.Size(342, 411);
+            this.txtOutput.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(718, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtInput);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox txtInput;
+        private Label label1;
+        private Label label2;
+        private TextBox txtOutput;
+    }
+}
---
+++ TyperConverter/Form1.cs
@@ -0,0 +1,47 @@
+﻿namespace TyperConverter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txtInput_TextChanged(object sender, EventArgs e)
+        {
+            txtOutput.Text = "^j::" + Environment.NewLine;
+            foreach (string line in txtInput.Text.Split("\r\n"))
+            {
+                foreach (var c in line)
+                {
+                    ReplaceAndSend(c.ToString());
+                }
+                ReplaceAndSend(Environment.NewLine);
+                //txtOutput.AppendText($"Send !+f{Environment.NewLine}");
+            }
+            File.WriteAllText(Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\calistir.ahk", txtOutput.Text);
+        }
+
+        private void ReplaceAndSend(string s)
+        {
+            string cmd = "SendRaw";
+            Dictionary<string, string> dic = new() 
+            {
+                { "\r\n", "Enter" },
+                { "\n", "Enter" },
+            };
+
+            if (dic.ContainsKey(s))
+            {
+                s = "{" + dic[s] + "}";
+            }
+            else
+            {
+                s = "{U+" + Convert.ToString((int)s[0], 16).PadLeft(4, '0') + "}";
+            }
+
+            txtOutput.AppendText($"Send {s}{Environment.NewLine}");
+            txtOutput.AppendText($"Sleep {10}{Environment.NewLine}");
+        }
+    }
+}
